690e0ea4a7403b68b049b7c157d274e9f4a1b1b1
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-10-01 04:58:50 +0900

    Initial commit

---
+++ lib/ex_stars.ex
@@ -0,0 +1,18 @@
+defmodule ExSTARS do
+  @moduledoc """
+  Documentation for `ExSTARS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSTARS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_stars_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSTARSTest do
+  use ExUnit.Case
+  doctest ExSTARS
+
+  test "greets the world" do
+    assert ExSTARS.hello() == :world
+  end
+end
